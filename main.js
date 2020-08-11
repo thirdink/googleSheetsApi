7d132f28e6fd30af08be34c04f01e731ad813fba
Rumit Tuladhar <rumitwashere@gmail.com>
2020-08-11 16:07:19 +0930

    test connection success

---
+++ main.js
@@ -0,0 +1,39 @@
+const {google} = require('googleapis');
+const keys = require('./keys.json');
+
+const client = new google.auth.JWT(
+    keys.client_email,
+    null,
+    keys.private_key,['https://www.googleapis.com/auth/spreadsheets']
+);
+
+client.authorize((err,tokens)=>{
+    try {
+        console.log('connected !');
+        gsRun(client);
+    } catch (err) {
+        console.log(err);
+        return;
+    }
+})
+
+const gsRun= async (cl)=>{
+    const gsapi = google.sheets({version:'v4',auth: cl});
+
+    const opt = {
+        spreadsheetId: '1QnlSwfwIQXVkpbliDdmW2HU9q5ePSFgGszqlcU3pfKw',
+        range: 'Sheet1!A1:B5'
+    };
+
+    let data = await gsapi.spreadsheets.values.get(opt);
+    // console.log(data.data.values);
+
+    let dataArray = data.data.values;
+
+    let newDataArray = dataArray.map((r)=>{
+        r.push(r[0]+ '-'+ r[1]);
+        return r;
+    });
+
+    console.log(newDataArray);
+}
